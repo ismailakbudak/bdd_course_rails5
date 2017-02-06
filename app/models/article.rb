27ba22d1e86c7e75d480c89c3ad6c5f30514849c
İsmail Akbudak <ismail.akbudak@lab2023.com>
2017-02-06 15:12:44 +0300

    Implement listing articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   # Validations
   validates_presence_of :title, :body
 
+  default_scope { order(created_at: :desc) }
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,15 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
