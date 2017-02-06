04577dbca1c7ba4c2ecb2618fbf176311f134c75
İsmail Akbudak <ismail.akbudak@lab2023.com>
2017-02-06 15:59:47 +0300

    Implement showing articles detail

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,11 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-  
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
   
     def article_params
